70081a9b34d342c3c4cdb6f2c0d8b3bfd3ffafcc
marchel <marchel.adi@ti.ukdw.ac.id>
2021-11-11 21:15:17 +0700

    soal 3 python

---
+++ 3_E_71210782.py
@@ -0,0 +1,9 @@
+# fitur menambah catatan daftar belanja Dani
+a = input("Masukkan daftar belanja 1 : ")
+b = input("Masukkan daftar belanja 2 : ")
+c = input("Tambahkan data ke daftar belanja 1 : ")
+d = input("Tambahkan data ke daftar belanja 2 : ")
+print ("Daftar belanja 1 adalah" ,a +',', end="")
+print (c)
+print ("Daftar belanaj 2 adalah" ,a +',', end="")
+print (d)
